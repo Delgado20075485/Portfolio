bd13e928ba3ca15ddcfea1be5d3fbf41aeed730c
Aaron Delgado <aarondelgado@Aarons-MacBook-Pro.local>
2021-08-24 11:20:08 -0500

    live site

---
+++ docs/precache-manifest.4c0a1eafe8aaa0cc0b62f633284677d1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be40ba8fad8ba4e189689452b5be510",
+    "url": "/Delgado20075485/Portfolio/index.html"
+  },
+  {
+    "revision": "a110c30d18c73e7e3576",
+    "url": "/Delgado20075485/Portfolio/static/css/main.5b6b7b77.chunk.css"
+  },
+  {
+    "revision": "3275c2a99c5680dbdd9b",
+    "url": "/Delgado20075485/Portfolio/static/js/2.5a3542f6.chunk.js"
+  },
+  {
+    "revision": "0def066106b5692cd5c135dcafc5767d",
+    "url": "/Delgado20075485/Portfolio/static/js/2.5a3542f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a110c30d18c73e7e3576",
+    "url": "/Delgado20075485/Portfolio/static/js/main.94f0fdb4.chunk.js"
+  },
+  {
+    "revision": "7530df8e493d7e811bf9",
+    "url": "/Delgado20075485/Portfolio/static/js/runtime-main.ed9e8883.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/Delgado20075485/Portfolio/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/Delgado20075485/Portfolio/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/Delgado20075485/Portfolio/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/Delgado20075485/Portfolio/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/Delgado20075485/Portfolio/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/Delgado20075485/Portfolio/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/Delgado20075485/Portfolio/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "a13f077b5c6b69edeb298466f43902a3",
+    "url": "/Delgado20075485/Portfolio/static/media/features-split-image-01.a13f077b.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/Delgado20075485/Portfolio/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "3d199183154335deba3411bfcdb37d3c",
+    "url": "/Delgado20075485/Portfolio/static/media/features-split-image-03.3d199183.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/Delgado20075485/Portfolio/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/Delgado20075485/Portfolio/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/Delgado20075485/Portfolio/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/Delgado20075485/Portfolio/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -119,13 +119,13 @@ const FeaturesSplit = ({
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  Lightning fast workflow
+                  About
                   </div>
                 <h3 className="mt-0 mb-12">
-                  Data-driven insights
+                  Redfounder
                   </h3>
                 <p className="m-0">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Redfounder came to me when I was a little kid trying to think of a name for a gaming channel. Little did I realize that it would turn into a launchpad channel.
                   </p>
               </div>
               <div className={
