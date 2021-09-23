301b2144b508cd2b73cfb2146846c87197e8757c
Aaron Delgado <aarondelgado@Aarons-MacBook-Pro.local>
2021-09-23 11:50:04 -0500

    edit

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -66,7 +66,7 @@ const FeaturesSplit = ({
 
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
-                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                <div className="ice-cream">
                   What is a
                   </div>
                 <h3 className="mt-0 mb-12">
@@ -92,7 +92,7 @@ const FeaturesSplit = ({
 
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-right" data-reveal-container=".split-item">
-                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                <div className="hide">
                   Launchpad
                   </div>
                 <h3 className="mt-0 mb-12">
@@ -118,7 +118,7 @@ const FeaturesSplit = ({
 
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
-                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                <div className="hilo">
                   About
                   </div>
                 <h3 className="mt-0 mb-12">
--- src/views/EasterEgg.js
+++ src/views/EasterEgg.js
@@ -3,11 +3,18 @@ import React from 'react'
 const EasterEgg = () => {
     return (
         <div class= "easter-egg-avocado">
-            Project Files
+        
         <div class= "inner-avocado">
-            
+            Download your favorite projects
           </div>
+          <div class= "robin">
+              The Kid LAROI - Addison Rae (Launchpad Cover)
+          </div>
+    <div class= "hill">
+        Project Files
+    </div>
         </div>
+
     );
 };
 
